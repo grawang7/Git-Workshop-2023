b9c761b94b257ae31bc30dc350d32b1b3036d69f
Megan <uyeharam@uci.edu>
2023-11-02 20:00:37 -0700

    Modified functions

--- git_workshop.py
+++ git_workshop.py
@@ -2,18 +2,16 @@ import math
 
 # all functions should take in a numerical value and return a numerical value
 # rename your function
-def func1(num_a):
-    # placeholder return, replace with your own function
-    return -1
+def add_one(num_a):
+    num_a += 1
 
-def func2(num_b):
-    # placeholder return, replace with your own function
-    return -1
+def subtract_one(num_b):
+    num_b -= 1
 
 # both people should modify this function
-def func3(num_a, num_b):
-    # do something with inputs and print something
-    print("placeholder output")
+def add_num(num_a, num_b):
+    num_c = num_a + num_b
+    print(num_c)
 
 a = 1
 b = 1
