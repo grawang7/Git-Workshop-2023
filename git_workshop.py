66a663fe83fbe3c076911beb5d39aa8d73a1b5b9
Grace Wang <gwang7@uci.edu>
2023-11-02 19:55:45 -0700

    Modified functions

--- git_workshop.py
+++ git_workshop.py
@@ -4,16 +4,17 @@ import math
 # rename your function
 def func1(num_a):
     # placeholder return, replace with your own function
-    return -1
+    return -1 - num_a
 
 def func2(num_b):
     # placeholder return, replace with your own function
-    return -1
+    return -1 + num_b
 
 # both people should modify this function
 def func3(num_a, num_b):
     # do something with inputs and print something
     print("placeholder output")
+    return num_a + num_b
 
 a = 1
 b = 1
